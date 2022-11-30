ad3b3b3c4764df3d5b1f13c98fa7bfe802688345
irvansyah98 <iervanfirdiansyah1998@gmail.com>
2022-11-30 23:08:50 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,9 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\AttendanceTask;
-use App\Attendance;
-use App\AttendanceLog;
 
 class HomeController extends Controller
 {
@@ -29,35 +26,4 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function autoCheckout(Request $request){
-        // var_dump(auth()->user()).die();
-        try {
-
-            $check = Attendance::where('status_type','checkin')->get();
-            foreach($check as $item){
-                $attendance = Attendance::where('user_id',$item->user_id)->where('status_type','checkin')->update([
-                    'notes' => 'Checkout Otomatis',
-                    'status_type' => 'checkout',
-                ]);
-
-                AttendanceLog::create([
-                    'user_id' => $item->user_id,
-                    'status_type' => 'checkout',
-                    'created_at' => new \DateTime,
-                    'updated_at' => new \DateTime,
-                ]);
-            }
-
-            return response()->json([
-                'success' => true,
-                'data' => '',
-                'message' => 'Success',
-            ]);
-        } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => $e->getMessage(),
-            ]);
-        }
-    }
 }
